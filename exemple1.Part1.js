489d4fd23df18a8487d54b5730b36939b1051b6a
dimatk01 <dimatk0101@gmail.com>
2022-10-23 15:48:26 +0300

    donne

--- exemple1.Part1.js
+++ exemple1.Part1.js
@@ -1,16 +1,31 @@
 function Product(ID, name, description, price, brand, sizes,
-    activeSize, quantity, date, review, images) {
-    let reviews = [review];
-    this.ID = ID;
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.brand = brand;
-    this.sizes = sizes;
-    this.activeSize = activeSize;
-    this.quantity = quantity
+    activeSize, quantity, date, images) {
+    let reviews = [];
+    if (typeof (ID) == "string")
+        this.ID = ID;
+    else throw new Error("please, check enter");
+    if (typeof (name) == "string")
+        this.name = name;
+    else throw new Error("please, check enter");
+    if (typeof (description) == "string")
+        this.description = description;
+    else throw new Error("please, check enter");
+    if (typeof (price) == "number")
+        this.price = price;
+    else throw new Error("please, check enter");
+    if (typeof (brand) == "string")
+        this.brand = brand;
+    else throw new Error("please, check enter");
+    if (typeof (sizes[0]) == "string")
+        this.sizes = sizes;
+    else throw new Error("please, check enter");
+    if (typeof (activeSize) == "string")
+        this.activeSize = activeSize;
+    else throw new Error("please, check enter");
+    if (typeof (quantity) == "number")
+        this.quantity = quantity
+    else throw new Error("please, check enter");
     this.date = date;
-    this.reviews = reviews;
     this.images = images;
 
     this.getID = function () {
@@ -73,12 +88,108 @@ function Product(ID, name, description, price, brand, sizes,
 
 
 
+    this.getReviewByID = function (id) {
+        if (id < 0) throw new Error("Check id!")
+        for (const review of reviews) {
+            if (review.id == id) {
+                return review
+            }
+        }
+    }
+    this.getImage = function (index) {
+        if (index < 0 | index > images.length - 1) throw new Error("Check index!")
+        if (index == undefined)
+            return images[0];
+        return images[index]
+    }
+    this.addSize = function (size) {
+        if (sizes.includes(size)) throw new Error("This size already exists!")
+        else sizes.push(size)
+    }
+    this.deleteSize = function (size) {
+        if (!sizes.includes(size) && typeof (size) == 'string') throw new Error("This size does not exists!")
+        else sizes.splice(sizes.indexOf(size), 1)
+    }
+    this.addReview = function (id, author, comment, rating) {
+        let review = new createReview(id, author, comment, rating)
+        reviews.push(review);
+    }
+    function createReview(id, author, comment, rating) {
+        this.id = id;
+        this.author = author;
+        this.date = new Date();
+        this.comment = comment;
+        this.rating = rating;
+    }
+    this.deleteReview = function (id) {
+        for (const review of reviews) {
+            if (review.id == id) {
+                reviews.splice(reviews.indexOf(review), 1)
+                break;
+            }
+        }
+    }
+    this.getAverageRating = function () {
+        let averageRating = 0;
+        for (const review of reviews) {
+            averageRating += review.rating++;
+        }
+        return averageRating / reviews.length
+    }
+
 
 }
-let newPro = new Product("01", "nike01", 'somethin', 0, "nike", ['s', 'm'], "s", 10, new Date(),
-    ["1", "Ted", new Date(), "nice", { servise: "5" }], ["img1"]);
-console.log(newPro.getPrise())
-console.log(newPro.setPrise(5))
-console.log(newPro.getPrise())
 
 
+let p1 = new Product("01", "nike01", "футболка для занять спортом",30, "nike", ['s', 'm'], "s", 10, new Date(), ["img1", "img2"]);
+let P2 = new Product("02", "adidas", "футбольні кросівки", 20, "nike", ['39', '40'], "40", 10, new Date(), ["img1", "img2"]);
+let products = [p1, P2]
+console.log(searchProducts(products, "футб"))
+console.log(sortProducts(products,"-name"))
+function searchProducts(products, search) {
+    let results = [];
+    for (const product of products) {
+        if (product.getDescription().indexOf(search) != -1) results.push(product)
+    }
+    return results
+}
+function sortProducts(products, sortAttribute){
+    let results = [];
+    switch (sortAttribute){
+        case"price": {
+            for (const product of products) {
+                results.push(product.getPrise())
+            }
+            return results.sort()
+        }
+        case"-price": {
+            for (const product of products) {
+                results.push(product.getPrise())
+            }
+            return results.sort().reverse()
+        }
+        case"ID": {
+            for (const product of products) {
+                results.push(product.getID())
+            }
+            return results.sort()
+        }
+        case"-ID": {
+            for (const product of products) {
+                results.push(product.getID())
+            }
+            return results.sort().reverse()
+        }
+        case"name": {
+            for (const product of products) {
+                results.push(product.getName())
+            }
+            return results.sort()
+        }case"-name": {
+            for (const product of products) {
+                results.push(product.getName())
+            }
+            return results.sort().reverse()
+        }
+    }
+}
